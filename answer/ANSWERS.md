252ae57f91834d32d930547d29c678a4846d4740
ejaytkm <ejaytkm@gmail.com>
2022-10-17 00:09:15 +0800

    Added asnwers

--- answer/ANSWERS.md
+++ answer/ANSWERS.md
@@ -1,10 +1,10 @@
 # QUESTION 1
-1. HTTP Request: `curl -X POST http://localhost:8000/api/cardgame/simulate -H "Content-Type: application/json" -d '{"noPlayers": 2}'`
+1. HTTP Request: `curl -X POST http://localhost:8000/api/cardgame/simulate -H "Content-Type: application/json" -d '{"noPlayers": 20}'`
 2. Frontend => navigate to localhost:8000
-3. Direct scripting. On root folder, call cardgame_simulate.php
 
 # QUESTION 2
-1. Create optimal indexes for id type of columns
-2. Change LIKE wildcard search to prefix to "Wildcard%" instaed of "%Wildcard%"
-3. SELECT TOP instead of OFFSET
+1. Where condition, certain wildcards like .name, change LIKE wildcard search prefix to "Wildcard
+
+
+2. Change LIKE wildcard search to prefix from "%キャビンアテンダント%" to "キャビンアテンダント%"
 4. Avoid OFFSET in LIMIT Clause - unecessary offset
--- answer/optimized_query.sql
+++ answer/optimized_query.sql
@@ -64,56 +64,75 @@ LEFT JOIN basic_abilities BasicAbilities
     AND (BasicAbilities.deleted) IS NULL)
 LEFT JOIN jobs_tools JobsTools
   ON Jobs.id = (JobsTools.job_id)
-LEFT JOIN affiliates Tools
-  ON (Tools.type = 1
-    AND Tools.id = (JobsTools.affiliate_id)
-    AND (Tools.deleted) IS NULL)
+-- LEFT JOIN affiliates Tools
+--   ON (Tools.type = 1
+--     AND Tools.id = (JobsTools.affiliate_id)
+--     AND (Tools.deleted) IS NULL)
 LEFT JOIN jobs_career_paths JobsCareerPaths
   ON Jobs.id = (JobsCareerPaths.job_id)
-LEFT JOIN affiliates CareerPaths
-  ON (CareerPaths.type = 3
-    AND CareerPaths.id = (JobsCareerPaths.affiliate_id)
-    AND (CareerPaths.deleted) IS NULL)
+-- LEFT JOIN affiliates CareerPaths
+--   ON (CareerPaths.type = 3
+--     AND CareerPaths.id = (JobsCareerPaths.affiliate_id)
+--     AND (CareerPaths.deleted) IS NULL)
 LEFT JOIN jobs_rec_qualifications JobsRecQualifications
   ON Jobs.id = (JobsRecQualifications.job_id)
-LEFT JOIN affiliates RecQualifications
-  ON (RecQualifications.type = 2
-    AND RecQualifications.id = (JobsRecQualifications.affiliate_id)
-    AND (RecQualifications.deleted) IS NULL)
+-- LEFT JOIN affiliates RecQualifications
+--   ON (RecQualifications.type = 2
+--     AND RecQualifications.id = (JobsRecQualifications.affiliate_id)
+--     AND (RecQualifications.deleted) IS NULL)
 LEFT JOIN jobs_req_qualifications JobsReqQualifications
   ON Jobs.id = (JobsReqQualifications.job_id)
-LEFT JOIN affiliates ReqQualifications
-  ON (ReqQualifications.type = 2
-    AND ReqQualifications.id = (JobsReqQualifications.affiliate_id)
-    AND (ReqQualifications.deleted) IS NULL)
+-- LEFT JOIN affiliates ReqQualifications
+--   ON (ReqQualifications.type = 2
+--     AND ReqQualifications.id = (JobsReqQualifications.affiliate_id)
+--     AND (ReqQualifications.deleted) IS NULL)
+LEFT JOIN affiliates Affiliate
+  ON (
+    (Affiliate.type = 1
+      AND Affiliate.id = (JobsTools.affiliate_id)
+      AND (Affiliate.deleted) IS NULL)
+    OR
+    (Affiliate.type = 3
+      AND Affiliate.id = (JobsCareerPaths.affiliate_id)
+      AND (Affiliate.deleted) IS NULL)
+    OR
+    (Affiliate.type = 2
+      AND Affiliate.id = (JobsRecQualifications.affiliate_id)
+      AND (Affiliate.deleted) IS NULL)
+    OR
+    (Affiliate.type = 2
+      AND Affiliate.id = (JobsReqQualifications.affiliate_id)
+      AND (Affiliate.deleted) IS NULL)
+  )
+
 INNER JOIN job_categories JobCategories
   ON (JobCategories.id = (Jobs.job_category_id)
     AND (JobCategories.deleted) IS NULL)
 INNER JOIN job_types JobTypes
   ON (JobTypes.id = (Jobs.job_type_id)
     AND (JobTypes.deleted) IS NULL)
-WHERE ((JobCategories.name LIKE 'キャビンアテンダント%'
-  OR JobTypes.name LIKE 'キャビンアテンダント%'
-  OR Jobs.name LIKE 'キャビンアテンダント%'
-  OR Jobs.description LIKE 'キャビンアテンダント%'
-  OR Jobs.detail LIKE 'キャビンアテンダント%'
-  OR Jobs.business_skill LIKE 'キャビンアテンダント%'
-  OR Jobs.knowledge LIKE 'キャビンアテンダント%'
-  OR Jobs.location LIKE 'キャビンアテンダント%'
-  OR Jobs.activity LIKE 'キャビンアテンダント%'
-  OR Jobs.salary_statistic_group LIKE 'キャビンアテンダント%'
-  OR Jobs.salary_range_remarks LIKE 'キャビンアテンダント%'
-  OR Jobs.restriction LIKE 'キャビンアテンダント%'
-  OR Jobs.remarks LIKE 'キャビンアテンダント%'
-  OR Personalities.name LIKE 'キャビンアテンダント%'
-  OR PracticalSkills.name LIKE 'キャビンアテンダント%'
-  OR BasicAbilities.name LIKE 'キャビンアテンダント%'
-  OR Tools.name LIKE 'キャビンアテンダント%'
-  OR CareerPaths.name LIKE 'キャビンアテンダント%'
-  OR RecQualifications.name LIKE 'キャビンアテンダント%'
-  OR ReqQualifications.name LIKE 'キャビンアテンダント%')
-  AND publish_status = 1
-  AND (Jobs.deleted) IS NULL)
+WHERE ((JobCategories.name LIKE '%キャビンアテンダント%'
+    OR JobTypes.name LIKE '%キャビンアテンダント%'
+    OR Jobs.name LIKE '%キャビンアテンダント%'
+    OR Jobs.description LIKE '%キャビンアテンダント%'
+    OR Jobs.detail LIKE '%キャビンアテンダント%'
+    OR Jobs.business_skill LIKE '%キャビンアテンダント%'
+    OR Jobs.knowledge LIKE '%キャビンアテンダント%'
+    OR Jobs.location LIKE '%キャビンアテンダント%'
+    OR Jobs.activity LIKE '%キャビンアテンダント%'
+    OR Jobs.salary_statistic_group LIKE '%キャビンアテンダント%'
+    OR Jobs.salary_range_remarks LIKE '%キャビンアテンダント%'
+    OR Jobs.restriction LIKE '%キャビンアテンダント%'
+    OR Jobs.remarks LIKE '%キャビンアテンダント%'
+    OR Personalities.name LIKE '%キャビンアテンダント%'
+    OR PracticalSkills.name LIKE '%キャビンアテンダント%'
+    OR BasicAbilities.name LIKE '%キャビンアテンダント%'
+    OR Affiliate.name LIKE '%キャビンアテンダント%'
+    OR Affiliate.name LIKE '%キャビンアテンダント%'
+    OR Affiliate.name LIKE '%キャビンアテンダント%'
+    OR Affiliate.name LIKE '%キャビンアテンダント%')
+    AND publish_status = 1
+    AND (Jobs.deleted) IS NULL)
 GROUP BY Jobs.id
 ORDER BY Jobs.sort_order desc,
 Jobs.id DESC LIMIT 50 OFFSET 0
